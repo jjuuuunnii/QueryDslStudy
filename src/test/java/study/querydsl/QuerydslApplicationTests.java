16d0fbec22b0053ca545d624723571712a4854e2
jjuuuunnii <jake991110@naver.com>
2024-01-18 14:16:58 +0900

    Feat: querydsl 설정, test

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,39 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertEquals(result, hello);
+
+	}
+
+}
